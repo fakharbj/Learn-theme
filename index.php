b0cd22f55feb08142fd59d2fedd6981824356ede
fakhar <fakhardev@gmail.com>
2024-07-10 13:45:41 +0500

    first commit for test

--- index.php
+++ index.php
@@ -6,11 +6,10 @@ get_search_form();
 
     if ( have_posts()) : 
         while( have_posts()):
-            ?>
-            <h1>hello world</h1>
-            // the_post();
-            // get_template_part('template-parts/post/content'); 
-        <?php
+         
+            the_post();
+            get_template_part('template-parts/post/content'); 
+       
             endwhile;
     endif;
 
